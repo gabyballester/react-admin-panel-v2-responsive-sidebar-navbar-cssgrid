42670232c7f72753f340ca1270f8f742ed750a43
Gabriel Ballester <gaby.ballester@gmail.com>
2021-05-08 14:11:01 +0200

    Create navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useState } from 'react';
+
+import Navbar from './components/navbar/Navbar'
+
 const App = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+
+  const openSidebar = () => {
+    setSidebarOpen(true)
+  }
+  
+  const closeSidebar = () => {
+    setSidebarOpen(false)
+  }
+
   return (
     <div className="container">
       <h1>React Dashboard</h1>
+      <Navbar sidebarOpen={sidebarOpen} openSidebar={openSidebar}/>
     </div>
   );
 }
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,33 @@
+import './Navbar.css';
+import avatar from '../../assets/avatar.svg';
+
+const Navbar = ({ openSidebar }) => {
+  return (
+    <div className="navBar">
+      <div
+        className="nav_icon"
+        onClick={() => openSidebar()}
+      >
+        <i className="fa fa-bars"></i>
+        <div className="navbar__left">
+          <a>Subscribers</a>
+          <a>Video Management</a>
+          <a className="active_link">Admin</a>
+        </div>
+        <div className="navbar__right">
+          <a>
+            <i className="fa fa-search"></i>
+          </a>
+          <a>
+            <i className="fa fa-clock-o"></i>
+          </a>
+          <a>
+            <img width="30" src={avatar} alt="avatar" />
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
